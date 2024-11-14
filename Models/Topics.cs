cbe80cbcefc9f5d4ddef9151def992545af06887
tbrooks16 <tyheir.brooks@its.ny.gov>
2024-11-14 08:55:05 -0500

    Setup pages for demo
    add tailwind config for tailwind intellisense
    
    Setup tailwind

---
+++ Models/Topics.cs
@@ -0,0 +1,10 @@
+namespace BlazorDemoPOC.Models
+{
+    public class Topics
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+    }
+
+}
